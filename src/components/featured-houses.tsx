217ed8d07c5adfc7539c6b6d1431b1d46ae7509d
Abdullah Ahmad <bdocoder@gmail.com>
2025-01-04 20:15:37 +0200

    add filtering to featured houses

--- src/components/featured-houses.tsx
+++ src/components/featured-houses.tsx
@@ -2,9 +2,37 @@ import { useState } from "react";
 import HouseCard from "./house-card";
 import SectionHeading from "./section-heading";
 import Subheading from "./subheading";
+import classNames from "classnames";
+
+function FilterButton({
+  selected,
+  ...props
+}: {
+  selected: boolean;
+} & React.ComponentProps<"button">) {
+  return (
+    <button
+      className={classNames(
+        "box-border flex items-center gap-2 px-6 border rounded-full",
+        selected
+          ? "bg-primary-light text-primary border-primary-light"
+          : "bg-white text-text-2 border-light"
+      )}
+      {...props}
+    />
+  );
+}
 
 export default function FeaturedHouses() {
   const [current, setCurrent] = useState(0);
+  const [filter, setFilter] = useState<"house" | "villa" | "apartment">(
+    "house"
+  );
+
+  function getMaxIndex() {
+    if (filter === "house") return 2;
+    return 1;
+  }
 
   return (
     <section className="pl-max">
@@ -14,23 +42,31 @@ export default function FeaturedHouses() {
           <SectionHeading>Featured House</SectionHeading>
         </div>
         <div className="flex justify-between gap-8 h-[52px]">
-          <button className="box-border flex items-center gap-2 px-6 border rounded-full bg-primary-light text-primary border-primary-light">
-            <img src="/house-green.svg" />
+          <FilterButton
+            selected={filter === "house"}
+            onClick={() => setFilter("house")}
+          >
             <span>House</span>
-          </button>
-          <button className="box-border flex items-center gap-2 px-6 bg-white border rounded-full text-text-2 border-light">
-            <img src="/villa.svg" />
+          </FilterButton>
+          <FilterButton
+            selected={filter === "villa"}
+            onClick={() => setFilter("villa")}
+          >
             <span>Villa</span>
-          </button>
-          <button className="box-border flex items-center gap-2 px-6 bg-white border rounded-full text-text-2 border-light">
-            <img src="/apartment.svg" />
+          </FilterButton>
+          <FilterButton
+            selected={filter === "apartment"}
+            onClick={() => setFilter("apartment")}
+          >
             <span>Apartment</span>
-          </button>
+          </FilterButton>
         </div>
         <div className="flex gap-4 h-[52px]">
           <button
             className="px-4 py-3 rounded-full bg-light"
-            onClick={() => setCurrent(Math.min(Math.max(0, current - 1), 6))}
+            onClick={() =>
+              setCurrent(Math.min(Math.max(0, current - 1), getMaxIndex()))
+            }
           >
             <div className="grid w-7 h-7">
               <img src="/chevron-left.svg" className="m-auto" />
@@ -38,7 +74,9 @@ export default function FeaturedHouses() {
           </button>
           <button
             className="px-4 py-3 text-white rounded-full bg-primary"
-            onClick={() => setCurrent(Math.min(Math.max(0, current + 1), 6))}
+            onClick={() =>
+              setCurrent(Math.min(Math.max(0, current + 1), getMaxIndex()))
+            }
           >
             <div className="grid w-7 h-7">
               <img src="/chevron-right.svg" className="m-auto" />
@@ -48,75 +86,88 @@ export default function FeaturedHouses() {
       </div>
 
       <div className="flex gap-10">
-        <HouseCard
-          style={{ left: `${-current * 380}px` }}
-          className="relative transition-[left]"
-          owner="dianne"
-          price="$ 35.000.000"
-          src="/featured-1.jpg"
-          title="Roselands House"
-          type="popular"
-        />
+        {filter === "house" && (
+          <HouseCard
+            style={{ left: `${-current * 380}px` }}
+            className="relative transition-[left]"
+            owner="dianne"
+            price="$ 35.000.000"
+            src="/featured-1.jpg"
+            title="Roselands House"
+            type="popular"
+          />
+        )}
 
-        <HouseCard
-          className="relative transition-[left]"
-          style={{ left: `${-current * 380}px` }}
-          owner="robert"
-          price="$ 20.000.000"
-          src="/featured-2.jpg"
-          title="Woodlandside"
-          type="new"
-        />
+        {filter === "house" && (
+          <HouseCard
+            className="relative transition-[left]"
+            style={{ left: `${-current * 380}px` }}
+            owner="robert"
+            price="$ 20.000.000"
+            src="/featured-2.jpg"
+            title="Woodlandside"
+            type="new"
+          />
+        )}
 
-        <HouseCard
-          className="relative transition-[left]"
-          style={{ left: `${-current * 380}px` }}
-          owner="ronald"
-          price="$ 44.000.000"
-          src="/featured-3.jpg"
-          title="The Old Lighthouse"
-          type="best"
-        />
+        {filter === "apartment" && (
+          <HouseCard
+            className="relative transition-[left]"
+            style={{ left: `${-current * 380}px` }}
+            owner="ronald"
+            price="$ 44.000.000"
+            src="/featured-3.jpg"
+            title="The Old Lighthouse"
+            type="best"
+          />
+        )}
 
-        <HouseCard
-          className="relative transition-[left]"
-          style={{ left: `${-current * 380}px` }}
-          owner="jenny"
-          price="$ 22.000.000"
-          src="/featured-4.jpg"
-          title="Cosmo's House"
-          type="popular"
-        />
+        {filter === "villa" && (
+          <HouseCard
+            className="relative transition-[left]"
+            style={{ left: `${-current * 380}px` }}
+            owner="jenny"
+            price="$ 22.000.000"
+            src="/featured-4.jpg"
+            title="Cosmo's House"
+            type="popular"
+          />
+        )}
 
-        <HouseCard
-          className="relative transition-[left]"
-          style={{ left: `${-current * 380}px` }}
-          owner="cameron"
-          price="$ 27.000.000"
-          src="/review-1.png"
-          title="Willow's Edge"
-          type="best"
-        />
+        {filter === "apartment" && (
+          <HouseCard
+            className="relative transition-[left]"
+            style={{ left: `${-current * 380}px` }}
+            owner="cameron"
+            price="$ 27.000.000"
+            src="/review-1.png"
+            title="Willow's Edge"
+            type="best"
+          />
+        )}
 
-        <HouseCard
-          className="relative transition-[left]"
-          style={{ left: `${-current * 380}px` }}
-          owner="ronald"
-          price="$ 19.000.000"
-          src="/post-2.jpg"
-          title="Starlight Haven"
-          type="new"
-        />
-
-        <HouseCard
-          className="relative transition-[left]"
-          style={{ left: `${-current * 380}px` }}
-          owner="courtney"
-          price="$ 31.000.000"
-          src="/post-1.jpg"
-          title="Seabreeze Manor"
-          type="new"
-        />
+        {filter === "house" && (
+          <HouseCard
+            className="relative transition-[left]"
+            style={{ left: `${-current * 380}px` }}
+            owner="ronald"
+            price="$ 19.000.000"
+            src="/post-2.jpg"
+            title="Starlight Haven"
+            type="new"
+          />
+        )}
+        {filter === "villa" && (
+          <HouseCard
+            className="relative transition-[left]"
+            style={{ left: `${-current * 380}px` }}
+            owner="courtney"
+            price="$ 31.000.000"
+            src="/review-2.png"
+            title="Seabreeze Manor"
+            type="new"
+          />
+        )}
       </div>
     </section>
   );
